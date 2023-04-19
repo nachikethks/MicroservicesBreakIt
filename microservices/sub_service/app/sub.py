8afe65b31b4f7cdc29b379d4cb4134b079cbdf32
maitreya526 <maitreyamagic@gmail.com>
2023-04-19 20:02:14 +0530

    Maitreya

---
+++ microservices/sub_service/app/sub.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+app.secret_key = 'thisisjustarandomstring'
+api = Api(app)
+
+class sub(Resource):
+    def get(self):
+        json = request.json
+        return {"result": json['num1']-json['num2']}
+    def post(self):
+        json = request.json
+        return {"result": json['num1']-json['num2']}
+
+api.add_resource(sub, '/sub')
+
+if __name__ == "__main__":
+    app.run(
+		debug=True,
+		port=5052,
+		host="0.0.0.0"
+	)
